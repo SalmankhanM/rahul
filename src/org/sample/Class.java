975559fbbfbcb4c3ba8eabce9896bb603c19c5b8
SalmankhanM <salmankhan9586@gmail.com>
2021-01-21 13:23:44 +0530

    done

---
+++ src/org/sample/Class.java
@@ -0,0 +1,22 @@
+package org.sample;
+
+public class Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubint a=1;
+		int b=2;
+		int c=3;
+		int d=4;
+		int e=5;
+		int f=6;
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+System.out.println("done");
+System.out.println(d);
+
+	}
+
+}
